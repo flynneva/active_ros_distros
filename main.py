3c7c411221cd5fbdc850396d18e3057afb75d20e
Evan Flynn <evanflynn.msu@gmail.com>
2023-11-05 08:32:45 -0800

    Switch to use rosdistro python package instead of snapshots server
    Signed-off-by: Evan Flynn <evanflynn.msu@gmail.com>

--- main.py
+++ main.py
@@ -1,47 +1,82 @@
 import logging
-import requests
-from bs4 import BeautifulSoup as bs
+import rosdistro
+import argparse
+# Replace with StrEnum after upgrading to > python 3.11
+from enum import Enum
+from typing import List
+
 
 ACTIVE_DISTROS_LOG_PATH = "/tmp/active_ros_distros.txt"
-
-# For every other ROS distro, if the distro has a `final` snapshot it is deprecated
-DISTRO_EXCEPTIONS = [
-    "noetic",
-]
-
-SNAPSHOTS_SITE = "http://snapshots.ros.org"
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("distro_info")
 
 
+parser = argparse.ArgumentParser(
+    prog="active_ros_distros",
+    description="List the active ROS distros for the specified configuration",
+)
+
+
+
+# Work around for string enums for python 3.10
+# From the enum docs:
+#   https://docs.python.org/3.10/library/enum.html#interesting-examples
+class StrEnum(Enum):
+    def __repr__(self):
+        return '<%s.%s>' % (self.__class__.__name__, self.name)
+
+
+# Replace with StrEnum after upgrading to > python 3.11
+class DistributionStatus(StrEnum):
+    ACTIVE = "active"
+    EOL = "end-of-life"
+
+
+# Replace with StrEnum after upgrading to > python 3.11
+class DistributionType(StrEnum):
+    ROS1 = "ros1"
+    ROS2 = "ros2"
+
+
 def main():
-    distros_page = requests.get(SNAPSHOTS_SITE)
-    distros_soup = bs(distros_page.content, "html.parser")
+    # Flags to specify exactly which ROS distro type to list (e.g. ROS 2 or ROS 1)
+    parser.add_argument("-t", "--distribution-type", default="")
+    parser.add_argument("-na", "--non-active", action="store_true")
 
-    distros = distros_soup.find("pre")
+    args = parser.parse_args()
 
-    active_distros = []
-    for distro in distros.find_all("a"):
-        if distro.text == "../":
-            continue
-        ros_distro = distro.text[:-1]
-        distro_page = requests.get(f"{SNAPSHOTS_SITE}/{distro.text}")
-        distro_soup = bs(distro_page.content, "html.parser")
-        distro_releases = [timestamp.text for timestamp in distro_soup.find("pre").find_all("a")]
+    distribution_status = DistributionStatus.ACTIVE.value
+    if args.non_active:
+        distribution_status = DistributionStatus.EOL.value
 
-        if ros_distro in DISTRO_EXCEPTIONS or "final/" not in distro_releases:
-            log.debug(f"{ros_distro} is still active")
-            active_distros.append(ros_distro)
-    
+    if args.distribution_type:
+        assert args.distribution_type in DistributionType._value2member_map_
+        distribution_type = args.distribution_type
+    else:
+        log.info("No distribution type specified, defaulting to only ROS 2 distros")
+        distribution_type = DistributionType.ROS2.value
+
+    index = rosdistro.get_index(rosdistro.get_index_url())
+
+    selected_distros = [
+        k
+        for k, v in index.distributions.items()
+        if v["distribution_type"] == distribution_type
+        if v["distribution_status"] == distribution_status
+    ]
+
+    write_distros_to_file(selected_distros)
+
+    return selected_distros
+
+
+def write_distros_to_file(selected_distros: List):
     log.info(f"Writing active distros to `{ACTIVE_DISTROS_LOG_PATH}`:")
     with open(ACTIVE_DISTROS_LOG_PATH, "w+") as txt_file:
-        for distro in active_distros:
+        for distro in selected_distros:
             log.info(f"\t- {distro}")
             txt_file.write(f"{distro}\n")
-    return active_distros
-        
-
 
 if __name__ == "__main__":
     main()
